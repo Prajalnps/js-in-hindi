6443be6f33880736fa1b9dec07b56c4a160e97b6
Prajaldev25 <128599282+Prajaldev25@users.noreply.github.com>
2024-01-10 10:47:31 +0545

    Data Types primitive and non primitive, assignment to read specification in ECMA script

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // Treat all JS code as newer version
+
+// like how you use alert() in browser, there is an alternative for node
+
+
+//code readability should be high
+console.log("Hwllo") 
+console.log("new")
+
+/*
+    Data Types
+
+    Primitive
+
+    number => 2 to the power 53
+    bigint
+    string => ""
+    boolean => true/false
+    null => standalone value, empty value
+    undefined =>
+    symbol => unique
+
+    Non Primitive 
+
+    object = 
+*/
+
+console.log(typeof "Prajal") //prints the type of string
+
+// problem of js
+console.log(typeof null); //prints object, error
