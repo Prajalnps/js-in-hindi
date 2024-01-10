94c060fc7bbeae562f3caeae40bd2d147c0b1224
Prajaldev25 <128599282+Prajaldev25@users.noreply.github.com>
2024-01-10 11:00:11 +0545

    file name change, Conversion of data types (number, boolean, string)

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,48 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof (score)) //you can also use the type of as method
+
+//if we want to do the operation in 100% number
+
+let valueInNumber = Number(score) //guarantee converted to number, class based approach
+
+console.log(typeof (valueInNumber));
+
+/*
+    Conversions
+
+    "33" => 33
+
+    "33abc" => NaN
+
+    true => 1
+
+    false => 0
+
+    null => 0
+
+    undefined => NaN
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+/*
+    Conversion
+
+    1 => true
+    0 => false
+    "" => false
+    "Prajal" => true
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
