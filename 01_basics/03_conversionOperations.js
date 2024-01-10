5ab5fed238da82e57799470839e3fd0748e81768
Prajaldev25 <128599282+Prajaldev25@users.noreply.github.com>
2024-01-10 15:58:52 +0545

    type conversion

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -46,3 +46,37 @@ let stringNumber = String(someNumber)
 
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+/***************************OPERATIONS*****************************/
+
+// important operations
+let value = 33
+
+let negValue = -value
+
+console.log(negValue)
+console.log(typeof negValue)
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 / 2)
+// console.log(2 ** 2) //2 to the power 2
+// console.log(2 % 2) // reminder is useful for cryptography, in algorithms
+
+let str1 = "hello"
+let str2 = " Prajal"
+
+let str3 = str1 + str2
+console.log(str3); //hello Prajal
+
+
+// Tricky conversion
+
+console.log(true) //output true
+console.log(+true) //output 1
+console.log(+"") //output 0
+
+// conversion
+// https://tc39.es/ecma262/#sec-type-conversion
